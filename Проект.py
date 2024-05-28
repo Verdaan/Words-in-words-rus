20f06940f9530b0abbcb89ba71f28d3e5c9ac58a
Verdaan <arseniy3420@gmail.com>
2024-05-28 23:50:30 +0300

    Update code
    Небольшие изменения в отображаемом тексте и смена названий переменных

--- Проект.py
+++ Проект.py
@@ -3,25 +3,26 @@ import re
 
 print("Введите слово от 2 до 8 букв:")
 input_word = input()
-primal_word = input_word.lower()
+original_word = input_word.lower()
+print("Загрузка. . .")
+
 final_word_list = []
 fixed_word_list = []
 
 with open("rus_words.txt", "r", encoding="utf-8") as f:
     word_list = f.readlines()                                  #Слова в списке имеют на конце "\n", которые надо убрать
     for words in word_list:
-        if words[0] not in primal_word:                        #Убираем очевидно неподходящие слова, снижая нагрузку на память
+        if words[0] not in original_word:                        #Убираем очевидно неподходящие слова, снижая нагрузку на память
             continue
         else:
             fixed_words = re.sub('\n', '', str(words))
             fixed_word_list.append(fixed_words)
 
 
-set_of_letters = list(permutations(primal_word))
+set_of_letters = list(permutations(original_word))
 
 
-if primal_word in fixed_word_list:
-    print("Загрузка. . .")
+if original_word in fixed_word_list:
     for i in set_of_letters:
         final_word = ""
         mid_word = ""
@@ -39,4 +40,7 @@ if primal_word in fixed_word_list:
                 else: continue
 else: print("Такого слова нет в словаре")
 
-print(final_word_list)
+if final_word_list == []:
+    print("Из этого слова невозможно собрать другие")
+else:
+    print(final_word_list)
